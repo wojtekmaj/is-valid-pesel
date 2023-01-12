edce8a2e33e82b5f5021b018fbdcaa783d594d22
Wojciech Maj <kontakt@wojtekmaj.pl>
2023-01-12 11:34:46 +0100

    Refactor code to make TypeScript happy

--- src/index.ts
+++ src/index.ts
@@ -15,13 +15,12 @@ export default function isValidPESEL(rawPesel: string | number): boolean {
 
   // calculate checksum
   let sum = 0;
-  for (let position = 0; position < weights.length; position += 1) {
-    const weight = weights[position];
-    const digit = parseInt(pesel[position], 10);
+  weights.forEach((weight, position) => {
+    const digit = Number(pesel[position]);
     sum += weight * digit;
-  }
+  });
 
   const checksum = (10 - (sum % 10)) % 10;
 
-  return checksum === parseInt(pesel[10], 10);
+  return checksum === Number(pesel[10]);
 }
